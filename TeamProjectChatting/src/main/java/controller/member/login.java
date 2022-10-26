f8db77573f6a54e66defb601192fc72e54a3f70d
504 <504@DESKTOP-A1HFHNV>
2022-10-26 18:20:13 +0900

    2022-10-26 pm6:17 login.jsp/css/java/memberDao-로그인메소드구현 부트스트랩사용/인풋박스/텍스트간격사용

--- TeamProjectChatting/src/main/java/controller/member/login.java
+++ TeamProjectChatting/src/main/java/controller/member/login.java
@@ -6,36 +6,37 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class login
- */
-@WebServlet("/login")
+import model.Dao.memberDao.memberDao;
+
+@WebServlet("/member/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String mid = request.getParameter("mid"); // http서버에 ajax로 통신한 아이디값 요청
+		String mpassword = request.getParameter("mpassword");// http서버에 ajax로 통신한 비밀번호값 요청
+		
+		System.out.println("아이디"+mid);System.out.println("비밀번호"+mpassword);
+		int result = memberDao.getInstance().login(mid, mpassword);
+
+		if (result == 1) { // 리턴값이 1이면 일치하는 아이디가 있으면 
+			HttpSession session = request.getSession();// 세션값 요청객체
+			
+			session.setAttribute("mid", mid); //세션에 mid값 저장 
+		}
+		System.out.println("서블렛..."+result);
+		response.getWriter().print(result); //결과 전송
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public login() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
 }
--- TeamProjectChatting/src/main/webapp/member_View/login.jsp
+++ TeamProjectChatting/src/main/webapp/member_View/login.jsp
@@ -4,9 +4,38 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>로그인 페이지</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link href="/TeamProjectChatting/CSS/member_CSS/login.css" rel="stylesheet">
 </head>
 <body>
-
+		
+		
+	<div class="box">
+		<p class="font-monospace title">We are cacaoTalk</p>
+		<p class="font-monospace title">Welcome.</p>
+		
+		<div class="form-floating mb-3">
+			  <input type="email" class="form-control mid" id="floatingInput" placeholder="name@example.com">
+ 				 <label for="floatingInput">Email </label>
+ 		</div>
+ 		
+ 		<div class="form-floating mb-3">
+ 			 <input type="password" class="form-control mpassword" id="floatingInput" placeholder="name@example.com">
+ 			 <label for="floatingInput">password </label>
+		</div>
+		
+			 <button type="button" class="btn btn-secondary btn-lg" onclick="login()">Enter</button>		
+		
+		<div class="font-monospace result">
+			<span style=""></span>
+		</div>
+	</div>
+		
+		
+	
+	<script  src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="/TeamProjectChatting/member_JS/login.js"></script>
 </body>
 </html>
